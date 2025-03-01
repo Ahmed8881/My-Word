bc6adbf2548c37eb45fdf82947e2bdc5b03eb8f9
Ahmed8881 <buttahmed2004@gmail.com>
2025-03-01 10:39:58 +0500

    add editor component with Quill editor integration, content management, and dark mode support

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { DocumentService } from '../services/document.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './editor.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrl: './editor.component.scss'
 })
 export class EditorComponent {
+  editorContent = ""
 
+  quillConfig = {
+    toolbar: false,
+    theme: "snow",
+    placeholder: "Start typing...",
+    modules: {
+      history: {
+        delay: 2000,
+        maxStack: 500,
+        userOnly: true,
+      },
+    },
+  }
+
+  constructor(private documentService: DocumentService) {}
+
+  ngOnInit() {
+    this.documentService.documentContent$.subscribe((content) => {
+      this.editorContent = content
+    })
+  }
+
+  onEditorCreated(editor: any) {
+    this.documentService.setEditor(editor)
+  }
+
+  onContentChanged(event: any) {
+    this.documentService.updateContent(event.html)
+  }
 }
