ecfd1c5543e7764c13f00d16aa9d958f98e2cb16
Ahmed8881 <buttahmed2004@gmail.com>
2025-03-01 10:34:37 +0500

    add customizable button component with variant, size, and icon support

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+  @Input() label = ""
+  @Input() icon = ""
+  @Input() variant: "primary" | "secondary" | "text" = "secondary"
+  @Input() size: "small" | "medium" | "large" = "medium"
+  @Input() disabled = false
 
+  @Output() clicked = new EventEmitter<void>()
+
+  onClick() {
+    if (!this.disabled) {
+      this.clicked.emit()
+    }
+  }
 }
