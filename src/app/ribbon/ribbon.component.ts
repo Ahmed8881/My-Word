6343bc7f4eae50cb4d631dd6e764a0ac46885bd8
Ahmed8881 <buttahmed2004@gmail.com>
2025-03-01 10:35:46 +0500

    add ribbon component with formatting options, font selection, and styling

--- src/app/ribbon/ribbon.component.ts
+++ src/app/ribbon/ribbon.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { DocumentService } from "../services/document.service"
 
 @Component({
-  selector: 'app-ribbon',
+  selector: "app-ribbon",
+  templateUrl: "./ribbon.component.html",
   standalone: true,
-  imports: [],
-  templateUrl: './ribbon.component.html',
-  styleUrl: './ribbon.component.scss'
+  styleUrls: ["./ribbon.component.scss"],
 })
 export class RibbonComponent {
+  fonts = ["Arial", "Calibri", "Times New Roman", "Verdana", "Georgia", "Tahoma"]
 
+  fontSizes = ["8", "9", "10", "11", "12", "14", "16", "18", "20", "22", "24", "26", "28", "36", "48", "72"]
+
+  selectedFont = "Calibri"
+  selectedFontSize = "11"
+
+  constructor(private documentService: DocumentService) {}
+
+  applyBold() {
+    this.documentService.applyFormatting("bold")
+  }
+
+  applyItalic() {
+    this.documentService.applyFormatting("italic")
+  }
+
+  applyUnderline() {
+    this.documentService.applyFormatting("underline")
+  }
+
+  applyStrikethrough() {
+    this.documentService.applyFormatting("strikethrough")
+  }
+
+  applySubscript() {
+    this.documentService.applyFormatting("subscript")
+  }
+
+  applySuperscript() {
+    this.documentService.applyFormatting("superscript")
+  }
+
+  applyTextColor(color: string) {
+    this.documentService.applyFormatting("color", color)
+  }
+
+  applyHighlight(color: string) {
+    this.documentService.applyFormatting("background", color)
+  }
+
+  applyAlignment(alignment: string) {
+    this.documentService.applyFormatting("align", alignment)
+  }
+
+  applyIndent(type: string) {
+    this.documentService.applyFormatting("indent", type)
+  }
+
+  applyList(type: string) {
+    this.documentService.applyFormatting("list", type)
+  }
+
+  changeFont(font: string) {
+    this.selectedFont = font
+    this.documentService.applyFormatting("font", font)
+  }
+
+  changeFontSize(size: string) {
+    this.selectedFontSize = size
+    this.documentService.applyFormatting("size", size)
+  }
 }
+
