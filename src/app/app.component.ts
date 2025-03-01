a0865941d58658e91cdd65ade28cb8c5d0b81837
Ahmed8881 <buttahmed2004@gmail.com>
2025-03-01 10:28:02 +0500

    add app component with dark mode support and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { DocumentService } from './services/document.service';
+import { ThemeService } from './services/theme.service';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "./header/header.component";
+import { RibbonComponent } from "./ribbon/ribbon.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { EditorComponent } from "./editor/editor.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, HeaderComponent, RibbonComponent, SidebarComponent, EditorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'MyWord';
+export class AppComponent implements OnInit {
+  isDarkMode = true
+
+  constructor(
+    private documentService: DocumentService,
+    private themeService: ThemeService,
+  ) {}
+
+  ngOnInit() {
+    this.themeService.isDarkMode$.subscribe((isDark) => {
+      this.isDarkMode = isDark
+    })
+  }
+
+  toggleTheme() {
+    this.themeService.toggleTheme()
+  }
+}
+
 }
